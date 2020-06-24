64667ff54d0cb631dfcae99cf5289904bd6516a6
superbeom <it.super.beom@gmail.com>
2020-06-24 13:08:12 +0900

    #10.1 Preloading Fonts and Assets

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  const [loaded, setLoaded] = useState(false);
+
+  const preLoad = async () => {
+    try {
+      /* Load Font */
+      await Font.loadAsync({
+        ...Ionicons.font,
+      });
+      /* Load Asset */
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded ? (
+    <View style={{ padding: 50 }}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
