d7af49b076555a899d6407e9b68cc15d1a7791f1
superbeom <it.super.beom@gmail.com>
2020-07-03 14:10:53 +0900

    #12.0 AuthHome

--- screens/Auth/AuthHome.js
+++ screens/Auth/AuthHome.js
@@ -1,15 +1,50 @@
 import React from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { View, Text } from "../config";
+import styled from "styled-components";
+import { View } from "../config";
+import constants from "../../constants";
 
-export default ({ navigation }) => (
-  <View>
-    <Text>Auth Home</Text>
-    <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-      <Text>Go to Login</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-      <Text>Go to Signup</Text>
-    </TouchableOpacity>
-  </View>
-);
+const Image = styled.Image`
+  width: ${constants.width}px;
+  margin-bottom: 50px;
+`;
+
+const Touchable = styled.TouchableOpacity``;
+
+const SignUpBtn = styled.View`
+  width: ${constants.width / 2}px;
+  background-color: ${(props) => props.theme.blueColor};
+  padding: 10px;
+  border-radius: 4px;
+  margin-bottom: 20px;
+`;
+
+const SignUpBtnText = styled.Text`
+  color: white;
+  text-align: center;
+  font-weight: 600;
+`;
+
+const LoginLink = styled.View``;
+
+const LoginLinkText = styled.Text`
+  color: ${(props) => props.theme.blueColor};
+  font-weight: 600;
+`;
+
+export default ({ navigation }) => {
+  return (
+    <View>
+      <Image resizeMode={"contain"} source={require("../../assets/logo.png")} />
+      <Touchable onPress={() => navigation.navigate("Signup")}>
+        <SignUpBtn>
+          <SignUpBtnText>Create New Account</SignUpBtnText>
+        </SignUpBtn>
+      </Touchable>
+      <Touchable onPress={() => navigation.navigate("Login")}>
+        <LoginLink>
+          <LoginLinkText>Log in</LoginLinkText>
+        </LoginLink>
+      </Touchable>
+    </View>
+  );
+};
